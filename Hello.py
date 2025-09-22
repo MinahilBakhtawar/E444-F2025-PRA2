cee78c18768eb24a3d1eb0305b3af766b0a49414
MinahilBakhtawar <minahil.bakhtawar@mail.utoronto.ca>
2025-09-22 17:28:21 -0400

    edited favicon and added timestamp

--- Hello.py
+++ Hello.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from datetime import datetime
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
@@ -22,6 +25,8 @@ def page_not_found(e):
 
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
 
 if __name__ == '__main__':
     app.run(debug=False) 
